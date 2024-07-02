e8de73e6893311b63c4cc54ee4b43fa1a03bf6d7
batistadaniel <109699716+batistadaniel@users.noreply.github.com>
2024-07-02 09:29:49 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+
+// Mostra o menu ao clicar no botao
+btnMenu.addEventListener('click', function() {
+    menu.classList.add('abrir-menu');
+});
+
+// Fecha o menu ao clicar no botao
+menu.addEventListener('click', function() {
+    menu.classList.remove('abrir-menu');
+});
+
+// Fecha o menu ao clicar fora
+overlay.addEventListener('click', function() {
+    menu.classList.remove('abrir-menu');
+});
